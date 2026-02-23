b4bae1b0aff8b896e9b7f4ba97e3c0b637743714
Guanhe Fu <f1016142506@gmail.com>
2026-02-23 11:42:55 -0500

    Update

--- app/src/main/java/com/codepath/lab6/MainActivity.kt
+++ app/src/main/java/com/codepath/lab6/MainActivity.kt
@@ -1,34 +1,28 @@
 package com.codepath.lab6
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
 import com.codepath.lab6.databinding.ActivityMainBinding
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
-import okhttp3.Headers
-import org.json.JSONException
 
-// Helper function for JSON parsing
 fun createJson() = Json {
     isLenient = true
     ignoreUnknownKeys = true
     useAlternativeNames = false
 }
 
-private const val TAG = "MainActivity/"
-private val API_KEY = BuildConfig.API_KEY
-private val PARKS_URL = "https://developer.nps.gov/api/v1/parks?api_key=${API_KEY}"
-
 class MainActivity : AppCompatActivity() {
 
-    private val parks = mutableListOf<Park>()
-    private lateinit var parksRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,44 +32,19 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // Find RecyclerView and set up adapter
-        parksRecyclerView = findViewById(R.id.parks)
-        val parksAdapter = ParksAdapter(this, parks)
-        parksRecyclerView.adapter = parksAdapter
+        val parksFragment: Fragment = ParksFragment()
+        val campgroundsFragment: Fragment = CampgroundFragment()
 
-        // Use LinearLayoutManager with a divider
-        parksRecyclerView.layoutManager = LinearLayoutManager(this).also {
-            val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
-            parksRecyclerView.addItemDecoration(dividerItemDecoration)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener{item ->
+            lateinit var fragment: Fragment
+            when(item.itemId){
+                R.id.nav_parks -> fragment = parksFragment
+                R.id.nav_campgrounds -> fragment = campgroundsFragment
+            }
+            replaceFragment(fragment)
+            true
         }
-
-        // Fetch data from NPS Parks API
-        val client = AsyncHttpClient()
-        client.get(PARKS_URL, object : JsonHttpResponseHandler() {
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
-            ) {
-                Log.e(TAG, "Failed to fetch parks: $statusCode")
-            }
-
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
-                Log.i(TAG, "Successfully fetched parks: $json")
-                try {
-                    val parsedJson = createJson().decodeFromString(
-                        ParksResponse.serializer(),
-                        json.jsonObject.toString()
-                    )
-                    parsedJson.data?.let { list ->
-                        parks.addAll(list)
-                        parksAdapter.notifyDataSetChanged()
-                    }
-                } catch (e: JSONException) {
-                    Log.e(TAG, "Exception: $e")
-                }
-            }
-        })
+        bottomNavigationView.selectedItemId = R.id.nav_parks
     }
 }
---
+++ app/src/main/java/com/codepath/lab6/ParksFragment.kt
@@ -0,0 +1,76 @@
+package com.codepath.lab6
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
+
+private const val TAG = "ParksFragment"
+private const val API_KEY = BuildConfig.API_KEY
+private const val PARKS_URL = "https://developer.nps.gov/api/v1/parks?api_key=${API_KEY}"
+class ParksFragment : Fragment() {
+    private val parks = mutableListOf<Park>()
+    private lateinit var parksRecyclerView: RecyclerView
+    private lateinit var parksAdapter: ParksAdapter
+
+    private fun fetchParks(){
+        val client = AsyncHttpClient()
+        client.get(PARKS_URL, object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers?,
+                json: JSON?
+            ) {
+                Log.i(TAG, "Successfully fetched parks: $json")
+                try{
+                    val parsedJson = createJson().decodeFromString(
+                        ParksResponse.serializer(),
+                        json?.jsonObject.toString()
+                    )
+                    parsedJson.data?.let{list->
+                        parks.addAll(list)
+                        parksAdapter.notifyItemInserted(list.size)
+                    }
+                } catch (e: JSONException){
+                    Log.e(TAG, "Exception: $e")
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "Failed to fetch parks: $statusCode")
+            }
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_parks, container, false)
+        val layoutManager = LinearLayoutManager(context)
+        parksRecyclerView = view.findViewById(R.id.parks)
+        parksRecyclerView.layoutManager = layoutManager
+        parksRecyclerView.setHasFixedSize(true)
+        parksAdapter = ParksAdapter(view.context, parks)
+        parksRecyclerView.adapter = parksAdapter
+        fetchParks()
+        return view
+    }
+}
